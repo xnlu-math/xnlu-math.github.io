661bf65ea6d1b33b31c6899a1c8ded1dd0fa30d6
xnlu-math <LU@LU-no-MacBook-Air.local>
2017-04-05 19:18:42 +0900

    update

--- conf2017.js
+++ conf2017.js
@@ -4,15 +4,19 @@ Conferences attended by Xiao-Nan LU in the academic year of 2017
 
 */
 
-var confName=["第３４回代数的組合せ論シンポジウム"]
+var confName=["第３４回代数的組合せ論シンポジウム", 
+				"日本数学会2017年度秋季総合分科会"]
 			
-var confPlace=["小山市立中央公民館・小山高専"]
+var confPlace=["小山市立中央公民館・小山高専",
+				"山形大学"]
 			
-var confData=["6月29日〜7月1日"]
+var confData=["6月29日〜7月1日",
+				"9月11日～14日"]
 			
-var confURL=[""]
+var confURL=["",
+				"http://mathsoc.jp/meeting/yamagata17sept/"]
 			
-var confRmks=[""]
+var confRmks=["", ""]
 			
 			function generateConfList2017(){
 				document.write("<ol>")
