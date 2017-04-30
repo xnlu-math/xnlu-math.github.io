9095d6028cb31eb275c09c152e00efe102453147
xnlu-math <macintosh.lu@gmail.com>
2017-05-01 00:36:10 +0900

    update TUS

--- conf2017.js
+++ conf2017.js
@@ -4,27 +4,31 @@ Conferences attended by Xiao-Nan LU in the academic year of 2017
 
 */
 
-var confName=["第３４回代数的組合せ論シンポジウム", 
+var confName=["日本品質管理学会第113回研究発表会",
+				"第３４回代数的組合せ論シンポジウム", 
 				"Japanese Conference on Combinatorics and its Applications (JCCA-2017)・離散数学とその応用研究集会2017",
 				"JCDCG^3 2017 (20th Japan Conference on Discrete and Computational Geometry, Graphs, and Games)",
 				"日本数学会2017年度秋季総合分科会"]
 			
-var confPlace=["小山市立中央公民館・小山高専", 
+var confPlace=["日本科学技術連盟・東高円寺ビル",
+				"小山市立中央公民館・小山高専", 
 				"熊本大学", 
 				"Tokyo University of Science (Kagurazaka)",
 				"山形大学"]
 			
-var confData=["2017年6月15日〜17日",
+var confData=["2017年5月27日",
+				"2017年6月15日〜17日",
 				"2017年8月20日〜22日",
 				"Aug 29 - Sept 1, 2017", 
 				"2017年9月11日～14日"]
 			
-var confURL=["",
+var confURL=["http://www.jsqc.org/q/news/events/113_program.pdf",
+				"",
 				"http://infoshako.sk.tsukuba.ac.jp/jcca/JCCA-2017/index.html",
 				"http://www.alg.cei.uec.ac.jp/itohiro/JCDCGG/",
 				"http://mathsoc.jp/meeting/yamagata17sept/"]
 			
-var confRmks=["", "", "", ""]
+var confRmks=["","", "", "", ""]
 			
 			function generateConfList2017(){
 				document.write("<ol>")
