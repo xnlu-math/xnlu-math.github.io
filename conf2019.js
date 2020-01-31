efc1e9e6e3e0134ae3c05323bf161502d92ed4b2
xnlu-math <macintosh.lu@gmail.com>
2020-01-31 19:27:57 +0900

    Update conf2019.js

--- conf2019.js
+++ conf2019.js
@@ -5,7 +5,9 @@ Conferences attended by Xiao-Nan LU in the academic year of 2019
 */
 
 var confName19=[
+	"The 6th Japan-Taiwan Conference on Combinatorics and its Applications (JTCCA)",
 	"日本数学会2020年度年会", 
+	"人工知能学会 第112回人工知能基本問題研究会(SIG-FPAI)",
 	"第16回組合せ論若手研究集会",
 /*	"2020 Taipei International Workshop on Combinatorics and Graph Theory",*/
 	"電子情報通信学会 情報理論研究会（IT）・信号処理研究会（SIP）・無線通信システム研究会（RCS）",
@@ -19,7 +21,9 @@ var confName19=[
 ]
 			
 var confPlace19=[
+	"熊本城ホール（熊本県）",
 	"日本大学駿河台キャンパス", 
+	"柳川市藤吉コミュニティセンター（福岡県）",
 	"慶應義塾大学矢上キャンパス", 
 /*	"Institute of Mathematics, Academia Sinica, Taipei, Taiwan",*/
 	"広島市青少年センター",
@@ -28,12 +32,14 @@ var confPlace19=[
 "龍谷大学瀬田キャンパス",
 "湯田温泉（山口県）",
 "金沢大学",
-"都城工業高等専門学校（宮崎県）", 
+"都城工業高等専門学校", 
 "University of Tokyo (Hongo Campus), Tokyo, Japan"
 ]
 			
 var confData19=[
+	"March 25-27, 2020",
 	"2020年3月16日〜19日",
+	"2020年3月8日〜9日",
 	"2020年2月27日〜28日", 
 /*	"Feb 6-8, 2020",*/
 	"2020年1月23日〜24日",
@@ -46,8 +52,9 @@ var confData19=[
 "May 27-30, 2019"
 ]
 			
-var confURL19=[
+var confURL19=["",
 	"https://mathsoc.jp/meeting/nichidai20mar/",
+	"https://sig-fpai.org/past/fpai112_cfp.html",
 	"http://www.comb.math.keio.ac.jp/wakate20/",
 /*	"https://www.math.sinica.edu.tw/www/file_upload/conference/202002Comb/Comb.html",*/
 	"https://www.ieice.org/ken/program/index.php?tgs_regid=0a96b5760f81cf3ba0c590264a4bf9bfd296a9e900905aee73bee06d69dbab54&tgid=IEICE-IT",
@@ -60,7 +67,7 @@ var confURL19=[
 "https://www.opt.mist.i.u-tokyo.ac.jp/hj2019/"
 ]
 			
-var confRmks19=["","","","","","“グループテスト入門”（招待講演）", "", "ミニシンポジウム「符号と暗号」世話人", ""
+var confRmks19=["Organizing Committee Member","","","","","","","","“グループテスト入門”（招待講演）", "", "ミニシンポジウム「符号と暗号」世話人", ""
 /*"“Locating arrays with error-correcting ability” (Contributed Talk) 7/16",*/]
 
 			
