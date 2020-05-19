428b68fa23641829115bee0fd9442c4326f03e16
xnlu-math <macintosh.lu@gmail.com>
2020-05-19 13:16:50 +0900

    Update conf2020.js

--- conf2020.js
+++ conf2020.js
@@ -3,23 +3,30 @@ Conferences attended by Xiao-Nan LU in the academic year of 2020
 */
 
 var confName20=[
-	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）"
+	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）",
+	"日本品質管理学会 第2回Webトーク",
+	"第1回整数論「Friday Tea Time Zoom Seminar」"
 ]
 			
 var confPlace20=[
-	"<del>東北学院大学 多賀城キャンパス</del>"
+	"<del>東北学院大学 多賀城キャンパス</del> Zoom", 
+	"Zoom",
+	"Zoom"
 ]
 			
 var confData20=[
-	"2020年5月28日〜29日"
+	"2020年5月28日〜29日",
+	"2020年5月20日, 10:30-11:30",
+	"2020年5月15日, 15:30-16:30"
 ]
 			
 var confURL20=[
-	"https://www.ieice.org/ken/program/index.php?mode=program&tgs_regid=84d6ca3005303698f18489b41d008d02e51886163760dfeb2aea4cc9cdeaa987&tgid=IT&layout=&lang="
+	"https://www.ieice.org/ken/program/index.php?mode=program&tgs_regid=84d6ca3005303698f18489b41d008d02e51886163760dfeb2aea4cc9cdeaa987&tgid=IT&layout=&lang=",
+	"",
+	"https://sites.google.com/view/fridayteatime/"
 ]
 			
-var confRmks20=[
-	"オンライン開催"
+var confRmks20=["", "", ""
 ]
 
 
