bed9079b72c872147bc15d7d1d30b54060922f08
xnlu-math <macintosh.lu@gmail.com>
2020-05-22 13:03:39 +0900

    goback update

--- goback.js
+++ goback.js
@@ -8,7 +8,7 @@ Go back by Xiao-Nan LU
 var myURL="https://xnlu-math.github.io/"
 
 function generateGoBack(){
-	document.write( '<a href="' + myURL + '" target="_top">Go back to Xiao-Nan Lu\'s homepage</a>')
+	document.write( '<p> [ <a href="' + myURL + '" target="_top">Go back to Xiao-Nan Lu\'s homepage</a> ] </p>')
 }
 
 function isHidden(oDiv){
--- organize.js
+++ organize.js
@@ -5,25 +5,30 @@ Organized Workshops by Xiao-Nan LU
 */
 
 
-var confPref=["<del>Organizing Committee Member of</del>",
-			"ミニシンポジウム「符号と暗号」 in ",
+var confPref=[
+			"",
+			"<del>Organizing Committee Member of</del>",
+			"",
 			"Organizing Committee Member of",
 			"",
 			"Organizing Committee Member of",
 			"",
 			""]
 			
-var confSuf=["",
-			"世話人",
+var confSuf=[
+			", 組織委員",
 			"",
-			"世話人",
+			", ミニシンポジウム「符号と暗号」 世話人",
+			"",
+			", 世話人",
 			"",
 			"世話人",
 			"世話人"]
 
 var confName=[
+			"Japanese Conference on Combinatorics and its Applications (JCCA-2020)・離散数学とその応用研究集会2020 (DMIA2020)・スペクトラルグラフ理論および周辺領域第9回研究集会 (SGT9)",
 			"<del>the 6th Japan-Taiwan Conference on Combinatorics and its Applications (JTCCA)</del>",
-			"「JCCA-2019・離散数学とその応用研究集会2019・スペクトラルグラフ理論および周辺領域 第8回研究集会」",
+			"Japanese Conference on Combinatorics and its Applications (JCCA-2019)・離散数学とその応用研究集会2019 (DMIA2019)・スペクトラルグラフ理論および周辺領域第8回研究集会 (SGT8)",
 			"the 4th Japan-Sino Symposium on Graph Theory, Combinatorics and Their Applications",
 			"研究集会「実験計画法ならびに情報数理と関連する組合せ構造2018」",
 			"the 5th Taiwan-Japan Conference on Combinatorics and its Applications (5th TJCCA)",
@@ -31,8 +36,9 @@ var confName=[
 			"研究集会「実験計画法およびその周辺の組合せ構造2014」"]
 			
 var confPlace=[
+			"<del>高知大学</del> オンライン開催予定",
 			"<del>Kumamoto-Jo Hall, Kumamoto, Japan</del>",
-			"都城工業高等専門学校（宮崎県）",
+			"都城工業高等専門学校",
 			"Tokyo University of Science（東京理科大学）, Japan",
 			"神戸大学",
 			"National Taiwan Normal University（國立臺灣師範大學）, Taiwan",
@@ -40,6 +46,7 @@ var confPlace=[
 			"城崎国際アートセンター（兵庫県）"]
 	
 var confData=[
+			"2020年8月18～20日", 
 			"<del>March 25-27, 2020</del>. Cancelled",
 			"2019年8月27日～29日",
 			"Nov 2-4, 2018",
@@ -48,7 +55,9 @@ var confData=[
 			"2017年11月23日～25日", 
 			"2014年12月13日～15日"]
 			
-var confURL=["https://sites.google.com/site/jtcca2020/",
+var confURL=[
+			"",
+			"https://sites.google.com/site/jtcca2020/",
 			"https://sites.google.com/site/jccadmiasg2019/",
 			"https://www.ed.tus.ac.jp/1416701/japan_sino_combin/",
 			"https://sites.google.com/view/doe2018/",
@@ -56,7 +65,9 @@ var confURL=["https://sites.google.com/site/jtcca2020/",
 			"https://sites.google.com/view/yugawara2017",
 			"http://www.ist.aichi-pu.ac.jp/~hirao/kinosaki2014/kinosaki2014.html"]
 			
-var confRmks=["日本側組織委員：千葉周也, 籾原幸二, 城本啓介, 神保雅一, <u>盧暁南</u>, 繆瑩, 三嶋美和子, 太田克弘; <br>　台湾側組織委員：陳宏賓, 傅恆霖",
+var confRmks=[
+			"ミニシンポジウム「符号と暗号」世話人：繆瑩, <u>盧暁南</u>, 佐竹翔平",
+			"日本側組織委員：千葉周也, 籾原幸二, 城本啓介, 神保雅一, <u>盧暁南</u>, 繆瑩, 三嶋美和子, 太田克弘; <br>　台湾側組織委員：陳宏賓, 傅恆霖",
 			"ミニシンポジウム「符号と暗号」世話人：繆瑩, <u>盧暁南</u>",
 			"日本側組織委員：江川嘉美, 加納幹雄, 古谷倫貴, 川谷元, <u>盧暁南</u>, 趙智賢; 中国側組織委員：闫桂英",
 			"世話人：<u>盧暁南</u>, 佐竹翔平, 澤正憲",
