d6d104795621fec90898e2b84e4631f396a944f3
xnlu-math <macintosh.lu@gmail.com>
2020-06-12 01:27:06 +0900

    Update conf2020.js

--- conf2020.js
+++ conf2020.js
@@ -4,7 +4,9 @@ Conferences attended by Xiao-Nan LU in the academic year of 2020
 
 var confName20=[
 	"Japanese Conference on Combinatorics and its Applications (JCCA-2020)・離散数学とその応用研究集会2020 (DMIA2020)・スペクトラルグラフ理論および周辺領域第9回研究集会 (SGT9)",	
-	"IWOCA 2020 : 31st International Workshop on Combinatorial Algorithms",
+	"東北大学情報科学研究科 組合せ論セミナー", 
+	"2020年度人工知能学会全国大会",
+	"IWOCA 2020: 31st International Workshop on Combinatorial Algorithms",
 	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）",
 	"日本品質管理学会 第2回Webトーク",
 	"第1回整数論「Friday Tea Time Zoom Seminar」"
@@ -12,6 +14,8 @@ var confName20=[
 			
 var confPlace20=[
 	"<del>高知大学</del> Zoom",
+	"Google Meet",
+	"<del>熊本城ホール</del> Zoom",
 	"<del>Bordeaux, France</del> Zoom",
 	"<del>東北学院大学 多賀城キャンパス</del> Zoom", 
 	"Zoom",
@@ -20,6 +24,8 @@ var confPlace20=[
 			
 var confData20=[
 	"2020年8月18～20日", 
+	"2020年6月19日, 16:00-17:00", 
+	"2020年6月9日〜12日",
 	"June 8-10, 2020",
 	"2020年5月28日〜29日",
 	"2020年5月20日, 10:30-11:30",
@@ -28,13 +34,15 @@ var confData20=[
 			
 var confURL20=[
 	"https://sites.google.com/view/jcca2020-dmia2020-sgt9",
+	"http://www.math.is.tohoku.ac.jp/~combin/",
+	"https://www.ai-gakkai.or.jp/jsai2020/",
 	"https://iwoca2020.labri.fr/",
 	"https://www.ieice.org/ken/program/index.php?mode=program&tgs_regid=84d6ca3005303698f18489b41d008d02e51886163760dfeb2aea4cc9cdeaa987&tgid=IT&layout=&lang=",
 	"",
 	"https://sites.google.com/view/fridayteatime/"
 ]
 			
-var confRmks20=["組織委員, ミニシンポジウム「符号と暗号」世話人", "", "", "", ""
+var confRmks20=["組織委員, ミニシンポジウム「符号と暗号」世話人","", "", "", "", "", ""
 ]
 
 
