7148606cd4c2f9f02c0d2c1c1d74c566adbd6c35
xnlu-math <macintosh.lu@gmail.com>
2021-04-02 13:25:03 +0900

    Update conf2021.js

--- conf2021.js
+++ conf2021.js
@@ -3,23 +3,27 @@ Conferences attended by Xiao-Nan LU in the academic year of 2021
 */
 
 var confName21=[
-	"大阪組合せ論セミナー"
+	"大阪組合せ論セミナー",
+	"2021年度第1回の早稲田大学整数論セミナー"
 ]
 			
 var confPlace21=[
 	"Zoom",
+	"Zoom"
 ]
 			
 var confData21=[
 	"2021年4月15日, 13:30-17:00",
-	
+	"2021年4月9日, 16:30-18:00"
 ]
 			
 var confURL21=[
-	"http://sv2-mat.ist.osaka-u.ac.jp/~higashitani/seminar.html"
+	"http://sv2-mat.ist.osaka-u.ac.jp/~higashitani/seminar.html",
+	"http://www.waseda.jp/sem-wnt/index.html"
 ]
 			
 var confRmks21=[
+	"",
 	""
 ]
 
