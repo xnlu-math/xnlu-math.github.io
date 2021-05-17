33bd5dbc3eeba42fe8dd0e3206aa8d52595e41b4
xnlu-math <macintosh.lu@gmail.com>
2021-05-17 16:40:49 +0900

    Updated confs

--- conf2020.js
+++ conf2020.js
@@ -38,7 +38,7 @@ var confName20=[
 	"IWOCA 2020: 31st International Workshop on Combinatorial Algorithms",
 	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）",
 	"日本品質管理学会 第2回Webトーク",
-	"第1回整数論「Friday Tea Time Zoom Seminar」"
+	"第1回「Friday Tea Time Zoom Seminar」"
 ]
 			
 var confPlace20=[
--- conf2021.js
+++ conf2021.js
@@ -3,6 +3,11 @@ Conferences attended by Xiao-Nan LU in the academic year of 2021
 */
 
 var confName21=[
+	"第37回代数的組合せ論シンポジウム",
+	"2021年度 人工知能学会全国大会",
+	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）",
+	"第33回「Friday Tea Time Zoom Seminar」",
+	"信阳师范学院数学与统计学院学术报告会",
 	"大阪組合せ論セミナー",
 	"2021年度第1回の早稲田大学整数論セミナー",
 	"CMSA (Center of Mathematical Sciences and Applications, Harvard) Math-Science Literature Lecture Series"
@@ -11,22 +16,42 @@ var confName21=[
 var confPlace21=[
 	"Zoom",
 	"Zoom",
+	"Zoom",
+	"Zoom",
+	"腾讯会议",
+	"Zoom",
+	"Zoom",
 	"Zoom"
 ]
 			
 var confData21=[
+	"2021年6月14日～16日",
+	"2021年6月8日～11日",
+	"2021年5月20日～21日",
+	"2021年5月14日, 15:30-16:30",
+	"2021年4月30日, 15:00-16:30",
 	"2021年4月15日, 13:30-17:00",
 	"2021年4月9日, 16:30-18:00",
 	"April 8, 2021, 9:00-10:30 ET"
 ]
 			
 var confURL21=[
+	"https://sites.google.com/view/2021-algcomb/",
+	"https://www.ai-gakkai.or.jp/jsai2021/",
+	"https://www.ieice.org/ken/program/index.php?tgs_regid=b89ac771658a1759bad9b5946c11b043e0c38a10dc1c7dfe10a0121327860683&tgid=IEICE-IT",
+	"https://sites.google.com/view/fridayteatime/",
+	"http://maths.xynu.edu.cn/info/1072/4748.htm",
 	"http://sv2-mat.ist.osaka-u.ac.jp/~higashitani/seminar.html",
 	"http://www.waseda.jp/sem-wnt/index.html",
 	"https://www.math.harvard.edu/event/math-science-literature-lecture-series-6-3-3-3-2-2-2-2-2-2-2/"
 ]
 			
 var confRmks21=[
+	"",
+	"",
+	"",
+	"",
+	"Invited talk: “Circulant almost orthogonal arrays and related problems in statistics and combinatorics” (in Chinese)", 
 	"",
 	"",
 	""
