967db258bcc9c875046cf1b960b163795f86eb37
xnlu-math <macintosh.lu@gmail.com>
2022-04-26 15:39:39 +0900

    Update conf2022.js

--- conf2022.js
+++ conf2022.js
@@ -3,22 +3,27 @@ Conferences attended by Xiao-Nan LU in the academic year of 2022
 */
 
 var confName22=[
+	"早稲田整数論セミナー",
 	"Catch-all Mathematical Colloquium of Japan"
 ]
 			
 var confPlace22=[
+	"Zoom", 
 	"Zoom"
 ]
 			
 var confData22=[
+	"2022年4月22日, 16:30-18:00",
 	"2022年4月11日, 10:00-12:00"
 ]
 			
 var confURL22=[
+	"http://www.waseda.jp/sem-wnt/",
 	"https://sites.google.com/view/catch-allmathematicscolloquium/schedule?authuser=0"
 ]
 			
 var confRmks22=[
+	"",
 	""
 ]
 
