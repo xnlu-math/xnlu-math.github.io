d3afe02ded8951665063bd6343c0d44cc5322f1d
xnlu-math <macintosh.lu@gmail.com>
2022-05-27 12:54:57 +0900

    Update conf2022.js

--- conf2022.js
+++ conf2022.js
@@ -3,26 +3,57 @@ Conferences attended by Xiao-Nan LU in the academic year of 2022
 */
 
 var confName22=[
+	"The 2022 IEEE International Symposium on Information Theory (ISIT)",
+	"第38回代数的組合せ論シンポジウム",
+	"2022年度 人工知能学会全国大会",
+	"Stinson66 - New Advances in Designs, Codes and Cryptography",
+	"International Conference on Coding and Cryptography 2022",
+	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）",
 	"早稲田整数論セミナー",
 	"Catch-all Mathematical Colloquium of Japan"
 ]
 			
 var confPlace22=[
+	"Aalto University, Finland",
+	"Zoom",
+	"国立京都国際会館 (Zoom配信あり)",
+	"The Fields Institute, Toronto (with virtual delivery)",
+	"Zoom (Delivered Online from Shanghai Jiao Tong University)",
+	"岐阜大学 サテライトキャンパス (Zoom配信あり)", 
 	"Zoom", 
 	"Zoom"
 ]
 			
 var confData22=[
+	"June 26-July 1, 2022",
+	"2022年6月16日～18日",
+	"2022年6月14日～17日",
+	"June 13-17, 2022", 
+	"May 27-29, 2022", 
+	"2022年5月17日～18日",
 	"2022年4月22日, 16:30-18:00",
 	"2022年4月11日, 10:00-12:00"
 ]
 			
 var confURL22=[
+	"https://www.isit2022.org/",
+	"https://sites.google.com/view/2022-symposium-algcombin",
+	"https://www.ai-gakkai.or.jp/jsai2022/",
+	"http://www.fields.utoronto.ca/activities/21-22/Stinson66",
+	"https://iisec.sjtu.edu.cn/info/1076/1170.htm",
+	"https://www.ieice.org/ken/program/index.php?tgs_regid=e3fe4e4e507fe427114d46ad78260f19bd61c15f7e210ca2f5af1dc2f50bfb3b&tgid=IEICE-IT",
 	"http://www.waseda.jp/sem-wnt/",
 	"https://sites.google.com/view/catch-allmathematicscolloquium/schedule?authuser=0"
 ]
 			
 var confRmks22=[
+	"“Error-Correcting Locating Arrays for Interaction Fault Location in Combinatorial Testing”", 
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
 	"",
 	""
 ]
