3b540c9f3491642de9ec4720f6cc66086158973e
xnlu-math <macintosh.lu@gmail.com>
2023-06-09 13:11:29 +0900

    Update conf2023.js

--- conf2023.js
+++ conf2023.js
@@ -3,26 +3,31 @@ Conferences attended by Xiao-Nan LU in the academic year of 2023
 */
 
 var confName23=[
+    "The 10th Slovenian International Conference on Graph Theory",
 	"NII共同研究「SAT技術を用いた組合せ遷移問題の解法に関する研究」第1回全体会合",
 	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）"
 ]
 			
 var confPlace23=[
+    "Kranjska Gora, Slovenia", 
     "神戸大学",
     "京都大学 楽友会館 (Zoom配信あり)"
 ]
 			
 var confData23=[
+    "June 18-24, 2023", 
     "5月22日〜23日",
     "5月11日〜12日"
 ]
 			
 var confURL23=[
+    "https://sicgt.si/",
     "",
     "https://ken.ieice.org/ken/program/index.php?tgs_regid=48be74bf3b9b592380289aa37d74ab8abf968167bbde5359cb324bc0fdd88bc7&tgid=IEICE-IT"
 ]
 			
 var confRmks23=[
+    "Contributed Talk in Minisymposium ‘Combinatorial Designs and their Applications’",
     "",
     ""
 ]
