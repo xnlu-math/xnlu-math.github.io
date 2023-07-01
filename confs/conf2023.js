3035b3b9410172df8c996e651e78f49dc5eac7f1
xnlu-math <macintosh.lu@gmail.com>
2023-07-02 08:20:26 +0900

    Update conf2023.js

--- confs/conf2023.js
+++ confs/conf2023.js
@@ -3,31 +3,36 @@ Conferences attended by Xiao-Nan LU in the academic year of 2023
 */
 
 var confName23=[
+	"電子情報通信学会 情報理論研究会（IT）",
     "The 10th Slovenian International Conference on Graph Theory",
 	"NII共同研究「SAT技術を用いた組合せ遷移問題の解法に関する研究」第1回全体会合",
 	"電子情報通信学会 情報理論研究会（IT）・マルチメディア情報ハイディング・エンリッチメント研究会（EMM）"
 ]
 			
 var confPlace23=[
+    "湘南工科大学",
     "Kranjska Gora, Slovenia", 
     "神戸大学",
     "京都大学 楽友会館 (Zoom配信あり)"
 ]
 			
 var confData23=[
+    "8月3日〜4日",
     "June 18-24, 2023", 
     "5月22日〜23日",
     "5月11日〜12日"
 ]
 			
 var confURL23=[
+    "https://ken.ieice.org/ken/program/index.php?mode=program&tgs_regid=2ff100216935555f3c8d48a678a463715fb99fc0d542b624e78a3f8db9d78875&tgid=IT",
     "https://sicgt.si/",
     "",
     "https://ken.ieice.org/ken/program/index.php?tgs_regid=48be74bf3b9b592380289aa37d74ab8abf968167bbde5359cb324bc0fdd88bc7&tgid=IEICE-IT"
 ]
 			
 var confRmks23=[
-    "Contributed Talk in Minisymposium ‘Combinatorial Designs and their Applications’",
+    "",
+    "“Circulant almost orthogonal arrays and related combinatorial designs” (Contributed Talk in Minisymposium ‘Combinatorial Designs and their Applications’) 6/22",
     "",
     ""
 ]
