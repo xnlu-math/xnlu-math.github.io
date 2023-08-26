16ad8d80a10329ecf77ad34969fa619996c780d1
xnlu-math <macintosh.lu@gmail.com>
2023-08-26 19:22:27 +0900

    Update conf2023.js

--- confs/conf2023.js
+++ confs/conf2023.js
@@ -3,6 +3,12 @@ Conferences attended by Xiao-Nan LU in the academic year of 2023
 */
 
 var confName23=[
+    "日本数学会 2023年度秋季総合分科会",
+    "電子情報通信学会 2023年度ソサイエティ大会",
+    "ワークショップ「エクスパンダーグラフの新しい構成手法の確立とその応用2」",
+    "NII共同研究「SAT技術を用いた組合せ遷移問題の解法に関する研究」第2回全体会合",
+    "Japanese Conference on Combinatorics and its Applications 2023 (離散数学とその応用研究集会2023)",
+    "第8回豊田理研異分野若手交流会",
 	"電子情報通信学会 情報理論研究会（IT）",
     "The 10th Slovenian International Conference on Graph Theory",
 	"NII共同研究「SAT技術を用いた組合せ遷移問題の解法に関する研究」第1回全体会合",
@@ -10,6 +16,12 @@ var confName23=[
 ]
 			
 var confPlace23=[
+    "東北大学 (川内北キャンパス)",
+    "名古屋大学 (東山キャンパス)",
+    "九州大学 (伊都キャンパス) とZoomによるハイブリッド開催",
+    "アクトシティ浜松 研修交流センター (浜松市)",
+    "愛知教育大学とオンライン (Zoom) のハイブリッド開催",
+    "ミッドランドホール (名古屋市) ",
     "湘南工科大学",
     "Kranjska Gora, Slovenia", 
     "神戸大学",
@@ -17,6 +29,12 @@ var confPlace23=[
 ]
 			
 var confData23=[
+    "9月20日〜23日",
+    "9月12日〜15日",
+    "9月4日〜5日",
+    "8月31日〜9月1日",
+    "8月28日〜30日",
+    "8月7日〜8日",
     "8月3日〜4日",
     "June 18-24, 2023", 
     "5月22日〜23日",
@@ -24,6 +42,12 @@ var confData23=[
 ]
 			
 var confURL23=[
+    "https://www.mathsoc.jp/activity/meeting/tohoku23sept/index.html",
+    "https://www.ieice.org/jpn_r/activities/taikai/society/2023/index.html",
+    "https://joint.imi.kyushu-u.ac.jp/post-9243/",
+    "",
+    "https://sites.google.com/view/jcca-dmia-2023/",
+    "https://www.toyotariken.jp/exchange/2023%e5%b9%b4%e5%ba%a6%e9%96%8b%e5%82%ac%e5%a0%b1%e5%91%8a/",
     "https://ken.ieice.org/ken/program/index.php?mode=program&tgs_regid=2ff100216935555f3c8d48a678a463715fb99fc0d542b624e78a3f8db9d78875&tgid=IT",
     "https://sicgt.si/",
     "",
@@ -31,7 +55,13 @@ var confURL23=[
 ]
 			
 var confRmks23=[
+    "“An algebraic sufficient condition for a circulant almost orthogonal array to be D-optimal” (統計数学分科会，一般講演) 9/23",
+    "企画セッション「組合せ論と情報理論 ー最新動向と展望ー」オーガナイザー \\\\“複数種類の陽性検体のための非適応型グループテスト: 組合せ構造と事後確率推定” (企画セッション) 9/14",
     "",
+    "",
+    "ミニシンポジウム「符号・暗号・人工知能」世話人",
+    "ポスター発表",
+    "“SAT技術を用いた複数画像からなる視覚暗号の構成” (発表者: 田光宏章) 8/4",
     "“Circulant almost orthogonal arrays and related combinatorial designs” (Contributed Talk in Minisymposium ‘Combinatorial Designs and their Applications’) 6/22",
     "",
     ""
