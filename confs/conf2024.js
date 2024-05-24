212c4624b8cd4f26c1934fbdf40903a567840e0a
xnlu-math <macintosh.lu@gmail.com>
2024-05-24 19:31:09 +0900

    Update conf2024.js

--- confs/conf2024.js
+++ confs/conf2024.js
@@ -3,26 +3,31 @@ Conferences attended by Xiao-Nan LU in the academic year of 2024
 */
 
 var confName24=[
-    "NII共同研究「SAT技術を用いた組合せ遷移問題の解法に関する研究」第4回全体会合",
-	"日本数学会2024年度年会"
+    "NII共同研究「解集合プログラミングの実用化に向けた並列解法に関する研究開発」第1回全体会合",
+    "2024年度人工知能学会全国大会",
+	"公開講演会「プライバシー保護とデータサイエンス」"
 ];
 
 var confPlace24=[
-    "長良川国際会議場 (岐阜市)",
-    "大阪公立大学 杉本キャンパス"
+    "松本商工会議所(長野県松本市)",
+    "アクトシティ浜松(静岡県浜松市)・オンライン",
+    "統計数理研究所・オンライン"
 ];
 			
 var confData24=[
-    "3月28日〜29日",
-    "5月11日〜12日"
+    "6月26日～27日",
+    "5月28日～31日",
+    "5月24日"
 ];
 			
 var confURL24=[
-    "",
-    ""
+    "https://nabesima.github.io/nii2024/",
+    "https://www.ai-gakkai.or.jp/jsai2024/",
+    "https://www.ism.ac.jp/openhouse/2024/index.html"
 ];
 			
 var confRmks24=[
+    "",
     "",
     ""
 ];
