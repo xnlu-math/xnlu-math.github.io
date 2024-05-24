f056655542239593496a9f900986a3e5a96aa3e5
xnlu-math <macintosh.lu@gmail.com>
2024-05-24 18:32:42 +0900

    modify goback

--- goback.js
+++ goback.js
@@ -7,9 +7,26 @@ Go back by Xiao-Nan LU
 
 var myURL="https://xnlu-math.github.io/"
 
+/*
 function generateGoBack(){
 	document.write( '<p> [ <a href="' + myURL + '" target="_top">Go back to Xiao-Nan Lu\'s homepage</a> ] </p>')
 }
+*/
+
+function generateGoBack() {
+    var goBackParagraph = document.createElement('p');
+    var link = document.createElement('a');
+    link.href = myURL;
+    link.target = '_top';
+    link.textContent = "Go back to Xiao-Nan Lu's homepage";
+    goBackParagraph.appendChild(document.createTextNode('[ '));
+    goBackParagraph.appendChild(link);
+    goBackParagraph.appendChild(document.createTextNode(' ]'));
+    
+    var destinationElement = document.getElementById('goBackContainer'); // Change 'goBackContainer' to the ID of the element where you want to append the link
+    destinationElement.appendChild(goBackParagraph);
+}
+
 
 function isHidden(oDiv){
 	var vDiv = document.getElementById(oDiv);
