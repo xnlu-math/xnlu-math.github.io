dd6292ef881c7eba8d6264c16a64ab53cf6e2f87
xnlu-math <macintosh.lu@gmail.com>
2024-07-28 17:34:33 +0900

    Update conf2024.js

--- confs/conf2024.js
+++ confs/conf2024.js
@@ -3,30 +3,60 @@ Conferences attended by Xiao-Nan LU in the academic year of 2024
 */
 
 var confName24=[
+    "電子情報通信学会 2024年度ソサイエティ大会",
+    "The 26th Japan Conference on Discrete and Computational Geometry, Graphs, and Games (JCDCG^3)",
+    "日本数学会 2024年度秋季総合分科会",
+    "第８回日本組合せゲーム理論研究集会",
+    "RIMS 共同研究「組合せ最適化セミナー」",
+    "第7回金沢暗号理論勉強会",
     "NII共同研究「解集合プログラミングの実用化に向けた並列解法に関する研究開発」第1回全体会合",
     "2024年度人工知能学会全国大会",
 	"公開講演会「プライバシー保護とデータサイエンス」"
 ];
 
 var confPlace24=[
+    "日本工業大学",
+    "Tokyo University of Science (Kagurazaka Campus)",
+    "大阪大学 豊中キャンパス",
+    "名古屋大学・オンライン",
+    "京都大学",
+    "石川県政しいのき迎賓館・オンライン",
     "松本商工会議所(長野県松本市)",
     "アクトシティ浜松(静岡県浜松市)・オンライン",
     "統計数理研究所・オンライン"
 ];
 			
 var confData24=[
+    "9月10日～13日",
+    "Sept 10 - 12, 2024", 
+    "9月3日～6日",
+    "8月23日～24日",
+    "8月6日～8日",
+    "7月26日～27日",
     "6月26日～27日",
     "5月28日～31日",
     "5月24日"
 ];
 			
 var confURL24=[
+    "https://www.ieice.org/jpn_r/activities/taikai/society/2024/",
+    "https://sites.google.com/view/jcdcg2024",
+    "https://www.mathsoc.jp/activity/meeting/osaka24sept/index.html",
+    "https://sites.google.com/view/jcgtw/%E7%A0%94%E7%A9%B6%E9%9B%86%E4%BC%9A",
+    "https://www.kurims.kyoto-u.ac.jp/coss/coss2024/coss2024.html",
+    "https://sites.google.com/view/kanazawacryptgraphy2024/",
     "https://nabesima.github.io/nii2024/",
     "https://www.ai-gakkai.or.jp/jsai2024/",
     "https://www.ism.ac.jp/openhouse/2024/index.html"
 ];
 			
 var confRmks24=[
+    "",
+    "“Cop number and capture time on incidence graphs of Steiner 2-designs” (Contributed Talk) 9/11",
+    "",
+    "",
+    "",
+    "",
     "",
     "",
     ""
