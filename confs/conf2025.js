555bdefcfe5782a2ffdc5018a352560e1cb15c83
xnlu-math <macintosh.lu@gmail.com>
2025-05-16 10:51:06 +0900

    Update conf2025.js

--- confs/conf2025.js
+++ confs/conf2025.js
@@ -4,25 +4,30 @@ Conferences attended by Xiao-Nan LU in the academic year of 2025
 
 var confName25=[
     "The 26th Conference of the International Linear Algebra Society",
-    "2025年度人工知能学会全国大会"
+    "2025年度人工知能学会全国大会",
+    "New Developments in Combinatorics and Information Science (組合せ論と情報科学の新展開)"
 ];
 
 var confPlace25=[
     "National Sun Yat-sen University (國立中山大學), Kaohsiung, Taiwan",
-    "大阪国際会議場（グランキューブ大阪）＋ オンライン"
+    "大阪国際会議場（グランキューブ大阪）＋ オンライン",
+    "京都大学 数理解析研究所"
 ];
 			
 var confData25=[
     "June 23 - 27, 2025",
-    "5月27日～30日"
+    "5月27日～30日",
+    "5月21日～23日"
 ];
 			
 var confURL25=[
     "https://ilas2025.tw/",
-    "https://www.ai-gakkai.or.jp/jsai2025/"
+    "https://www.ai-gakkai.or.jp/jsai2025/",
+    "https://sites.google.com/view/rims-math-info-2025/"
 ];
 			
 var confRmks25=[
     "Contributed talk at MS25: Enumerative/Algebraic Combinatorics and Matrices",
+    "",
     ""
 ];
