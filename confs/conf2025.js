c13798bb850286580b8afa65a3ce519d7129bcc1
xnlu-math <macintosh.lu@gmail.com>
2025-08-07 17:36:49 +0900

    Update conf2025.js

--- confs/conf2025.js
+++ confs/conf2025.js
@@ -3,31 +3,61 @@ Conferences attended by Xiao-Nan LU in the academic year of 2025
 */
 
 var confName25=[
+    "組合せ論サマースクール2025",
+    "日本数学会2025年度秋季総合分科会", 
+    "九大IMI共同利用研究「エクスパンダーグラフにまつわる数理科学と応用」", 
+    "Japanese Conference on Combinatorics and its Applications 2025 離散数学とその応用研究集会2025",
+    "電子情報通信学会 情報理論研究会 (IT)",
+    "大阪組合せ論セミナー",
     "The 26th Conference of the International Linear Algebra Society",
     "2025年度人工知能学会全国大会",
     "New Developments in Combinatorics and Information Science (組合せ論と情報科学の新展開)"
 ];
 
 var confPlace25=[
+    "大分県別府市 ふれあい広場 サザンクロス",
+    "名古屋大学",
+    "九州大学 伊都キャンパス・Zoom (オンライン参加)",
+    "広島YMCA 国際文化センター・Zoom (オンライン参加)",
+    "東京工業高等専門学校・Zoom (オンライン参加)",
+    "大阪公立大学文化交流センター", 
     "National Sun Yat-sen University (國立中山大學), Kaohsiung, Taiwan",
     "大阪国際会議場（グランキューブ大阪）＋ オンライン",
     "京都大学 数理解析研究所"
 ];
 			
 var confData25=[
+    "9月27日～30日",
+    "9月16日～19日",
+    "8月25日～27日",
+    "8月19日～21日",
+    "8月5日～6日",
+    "8月1日",
     "June 23 - 27, 2025",
     "5月27日～30日",
     "5月21日～23日"
 ];
 			
 var confURL25=[
+    "https://sites.google.com/view/cos2025/",
+    "https://www.mathsoc.jp/activity/meeting/nagoya25sept/",
+    "https://joint.imi.kyushu-u.ac.jp/post-17970/",
+    "https://sites.google.com/view/jcca-dmia-2025/home",
+    "https://ken.ieice.org/ken/program/index.php?tgs_regid=0245ed58df1f0a21cfc1acafe0217cc1f8c9f80fba062f68e530176e22219c3e&tgid=IEICE-IT",
+    "http://sv2-mat.ist.osaka-u.ac.jp/~higashitani/seminar.html",
     "https://ilas2025.tw/",
     "https://www.ai-gakkai.or.jp/jsai2025/",
     "https://sites.google.com/view/rims-math-info-2025/"
 ];
 			
 var confRmks25=[
-    "Contributed talk at MS25: Enumerative/Algebraic Combinatorics and Matrices",
+    "入門講義（招待講演）“組合せテスト入門 〜極値組合せ論・組合せデザイン・符号の交差点〜” 予定",
+    "一般講演 “Completely uniform nested pairings in Boolean Steiner quadruple systems” 9/16 予定",
+    "",
+    "",
+    "",
+    "",
+    "Contributed talk at MS25: Enumerative/Algebraic Combinatorics and Matrices: “Ternary circulant almost orthogonal arrays with (near) D-optimality and good binary sequence pairs” 6/26",
     "",
     ""
 ];
